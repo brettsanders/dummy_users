cbb7f3b5c76f3c670f2bce056cb7f9dc8964e3df
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 16:38:04 -0500

    created login and signup with varifications

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,21 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/login' do
+
+  @user = User.authenticate(params[:user][:email], params[:user][:password])
+
+  if @user 
+    @notification = "Logged In"
+  else
+    @notification = "Invalid Login"
+  end
+
+  erb :index
+end
+
+post '/signup' do 
+  User.create(params[:user])
+  erb :index
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  
+  def self.authenticate(email, password)
+    @user = User.find_by_email_and_password(email, password)
+    @user
+  end
+
+end
